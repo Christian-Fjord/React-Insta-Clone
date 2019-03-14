adfb0f0e5e86f82feb9ef272f4fcbcab62005bc8
Christian-Fjord <fjord.christian@gmail.com>
2019-03-13 21:31:15 -0500

    Initial Commit

---
+++ instagram-clone/instagram/components/searchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './searchBar.css';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
